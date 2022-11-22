e5723a70f4e9ca9c3f7571af11b8ea9d0cf1980a
Mogul <mogul27442@gmail.com>
2022-11-22 16:21:35 +0000

    Completed tutorial and removed build

---
+++ lumache.py
@@ -0,0 +1,16 @@
+def get_random_ingredients(kind=None):
+    """
+    Return a list of random ingredients as strings
+
+    :param kind: Optional "kind" of ingredients
+    :type kind: list[str] or None
+    :return: The ingredients list
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :rtype: list[str]
+    
+    """
+    return ["shells", "gorgonzola", "parsley"]
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
